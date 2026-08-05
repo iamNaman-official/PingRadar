a52366fe179a5e95e2561f39f494565bad054c6d
iamNaman-official <namanbagrecha085@gmail.com>
2026-08-05 13:53:28 +0530

    fix: add fallback secret key for CI

--- pingradar_project/settings.py
+++ pingradar_project/settings.py
@@ -24,7 +24,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', "django-insecure-dummy-secret-key-for-dev-only")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
